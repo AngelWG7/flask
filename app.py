33dd6d1992f472aa66d93bae160de6cefbb7a287
AngelW <20211016@uthh.edu.mx>
2024-06-05 15:47:12 -0600

    Nombre y grupo del alumno

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "¡Hola, mundo!"
+    return "Alumno: Angel Wenceslao Hernández Hernández<br>Grupo: '9A'"
 
 if __name__ == '__main__':
     app.run(debug=True)
